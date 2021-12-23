c8f061e75b04cfb743c5653f053dbf897aaa85a3
Mahamud Hasan <mahamud15-10467@diu.edu.bd>
2021-12-23 19:38:14 +0600

    first commit

---
+++ src/data.js
@@ -0,0 +1,33 @@
+const questions = [
+  {
+    id: 1,
+    title: 'Do I have to allow the use of cookies?',
+    info:
+      'Cookies are files created by sites you visit. They make your online experience easier by saving browsing information. With cookies, sites can',
+  },
+  {
+    id: 2,
+    title: 'How do I change my My Page password?',
+    info:
+      'follow the steps under Change Your Password then tap Forgot Password? and follow the steps to reset it. Keep in mind that you',
+  },
+  {
+    id: 3,
+    title: 'What is BankID?',
+    info:
+      'BankID is a personal and easy electronic credential for secure identification and signing online.',
+  },
+  {
+    id: 4,
+    title: 'Whose birth number can I use?',
+    info:
+      'In numerology, each digit from one to nine has certain characteristics.',
+  },
+  {
+    id: 5,
+    title: 'When do I recieve a password ordered by letter?',
+    info:
+      'In general, a password is an arbitrary string of characters including letters, digits, or other symbols.',
+  },
+]
+export default questions
